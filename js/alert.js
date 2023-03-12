e0c4cd521e3de81c5f431f70f690b69b295875ab
Rabia Yousuf <53699291+rabiayousuf187@users.noreply.github.com>
2023-03-12 17:49:24 +0500

    Assign 01
    make js functions to access each task

--- js/alert.js
+++ js/alert.js
@@ -2,5 +2,51 @@ function ques1(){
      // Greet Script for visitors
      var greet = "Hello! \nWelcome to Web Designing and Development Web.";
 
-     alert(greet); 
+     alert(greet);   
+}
+
+function ques2(){
+     // Enter a valid password.
+     var error = "Error! Please enter a valid password.";
+
+     alert(error);
+}
+
+function ques3(){
+        // Welcome to JS Land... Happy Coding!
+
+        var js_coding = "Welcome to JS Land... \nHappy Coding!";
+        
+        alert(js_coding);
+
+
+}
+function ques4(){
+        // messages in sequence:
+        var sequence = "Welcome to JS Land..." , 
+            prevent_dia = "Happy Coding!";              // dialog line cannot appear
+            
+        alert(sequence);
+        alert(prevent_dia);
+
+}
+function ques5(){
+        // messages in sequence:
+        var browser = "Hello... I can run through my web browser's console";
+        
+        alert(browser);
+
+}
+function ques6(){
+        console.log("window.location======", window.location);
+        let url = window.location.host + "/assign_6/index.html";
+        console.log("window.locatio host======", url);
+        window.open(url,"_blank");
+}
+function ques7(){
+        console.log("window.location======", window.location);
+        let url = "/assign_7/index.html";
+        // let url = window.location.host + "/assign_7/index.html";
+        console.log("window.locatio host======", url);
+        window.open(url,"_blank");
 }
